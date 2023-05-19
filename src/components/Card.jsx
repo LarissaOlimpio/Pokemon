fb43b8acb9e9af86b658a6e546be30bd41627b83
Larissa Olimpio <lrsolimpio@gmail.com>
2023-05-19 01:13:45 -0300

    removendo comentarioas desnecessarios

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,14 +14,3 @@ export default function Card({ pokemon }) {
     </div>
   )
 }
-/* {
-  
- <Image
-        src={` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-        width={120}
-        height={120}
-        alt={pokemon.name}
-      />
-https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png
- 
-} */
